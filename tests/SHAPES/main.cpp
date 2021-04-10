c0bd8126e4837ab6f64bfea4476e366d414f7d61
Damian Heer <damian@heer.one>
2021-04-10 12:17:12 +0200

    shapes test works

--- tests/SHAPES/main.cpp
+++ tests/SHAPES/main.cpp
@@ -26,198 +26,269 @@ template<class Vec>
     return dot(_v, _v);
 };
 
+[[nodiscard]] vec3 operator*(const mat4& _cam, const vec3& _vec) noexcept {
+    const vec4 t = _cam * vec4(_vec.x, _vec.y, _vec.z, 1.f);
+    return vec3(t.x, t.y, t.z);
+};
+
+struct Material {
+    Material(const vec3& _c) : col(_c) {}
+    vec3 col;
+};
+
+struct DirLight {
+    vec3 dir;
+    vec3 col;
+    float strength;
+};
+
+struct Ambiente {
+    vec3 col;
+    float strength;
+};
+
+class Camera {
+
+    vec3 dir;
+    vec3 up;
+    vec3 right;
+
+    const vec3 upAxis = vec3(0.f, 1.f, 0.f);
+
+public:
+
+    vec3 pos;
+    vec3 target;
+
+    const float near;
+    const float far;
+    const float fovX;
+    const float fovY;
+    const int64_t w;
+    const int64_t h;
+
+    void update() noexcept {
+        dir = normalize(target - pos);
+        right = normalize(cross(dir, upAxis));
+        up = normalize(cross(right, dir));
+    };
+
+    [[nodiscard]] Vec3& operator*(Vec3& _in) const noexcept {
+        _in = normalize(_in.x*right + _in.y*up + _in.z*dir);
+        return _in;
+    };
+
+    Camera(size_t _w, size_t _h, float _fov, float _near, float _far) noexcept
+        : w(_w), h(_h), fovX(_fov), fovY(_w / _h * _fov), near(_near), far(_far) {}
+
+};
+
+struct Ray {
+    vec3 orig;
+    vec3 dir;
+    vec3 end;
+};
+
 struct Sphere {
+    Sphere(float _r, const vec3& _c, const Material& _m) : 
+        radius(_r), centre(_c), mat(_m) {}
+
     float radius;
     vec3 centre;
+    Material mat;
 };
 
 struct Plane {
+    Plane(const vec3& _n, const vec3& _p0, float _hh, float _hw, const Material& _mat) :
+        n(_n), p0(_p0), hh(_hh), hw(_hw), mat(_mat) {}
+
     vec3 n;
     vec3 p0; //assumed the centre of the plane
-    vec2 hs; //halfsizes
+    float hh, hw;
+    Material mat;
 };
 
 struct Zylinder {
+    Zylinder(float _r, const vec3& _p1, const vec3& _p2, const Material& _mat) :
+        radius(_r), p1(_p1), p2(_p2), mat(_mat) {}
+
     float radius;
     vec3 p1;
     vec3 p2;
+    Material mat;
 };
 
-bool sphere(const Sphere& _s, const vec3& _ray_o, const vec3& _ray_d, vec3& _intersect, vec3& _normal) {
-    const vec3 oc = _ray_o - _s.centre;
-    const float d = dot(oc, _ray_d);
+bool sphere(const Sphere& _s, const Ray& _ray, vec3& _intersect, vec3& _normal, float& _t) {
+    const vec3 oc = _ray.orig - _s.centre;
+    const float d = dot(oc, _ray.dir);
     const float c = (dot(oc, oc) - _s.radius * _s.radius);
     if (c > 0.f && d > 0.f) return false;
     const float delta = d * d - c;
     if(delta < 0.f) return false;
-    float t = -d - sqrt(delta);
-    if (t < 0.0f) t = 0.0f;
-    _intersect = _ray_o + t*_ray_d;
+    _t = -d - sqrt(delta);
+    if (_t < 0.0f) _t = 0.0f;
+    _intersect = _ray.orig + _t*_ray.dir;
     _normal = normalize(vec3(_intersect - _s.centre));
     return delta >= 0.f;
 };
 
-bool plane(const Plane& _p, const vec3& _ray_o, const vec3& _ray_d, vec3& _intersect, vec3& _normal){
-    const float s = dot(_p.n, vec3(_p.p0 - _ray_o));
-    const float t = dot(_ray_d, _p.n);
-    const float d = t != 0.f ? s / t : 0.f; //todo 
-    _intersect = _ray_o + d * _ray_d;
+bool plane(const Plane& _p, const Ray& _ray, vec3& _intersect, vec3& _normal, float& _t){
+    const float s = dot(_p.n, vec3(_p.p0 - _ray.orig));
+    _t = dot(_ray.dir, _p.n);
+    const float d = _t != 0.f ? s / _t : 0.f; //todo 
+    _intersect = _ray.orig + d * _ray.dir;
     _normal = _p.n;
-    const float dist = distance(_intersect, _p.p0);
-    return d != 0.f && dist <= _p.hs[0] && dist <= _p.hs[1];
+    const vec3 dist = _intersect - _p.p0;
+    return d != 0.f && abs(dist.x) <= _p.hw && abs(dist.y) <= _p.hh;
 };
 
-// A + t*(sb-A) -> t:[0,1]
-bool zylinder(const Zylinder& _p, const vec3& _ray_o, const vec3& _ray_d, vec3& _intersect, vec3& _normal){   
-    const vec3 sa = _ray_o;
-    const vec3 sb = _ray_d * 100000.f;
-    const vec3 n = sb - sa;
-    const vec3 P = _p.p1;
-    const vec3 Q = _p.p2;
-    const vec3 d = Q - P;
-    const float r = _p.radius;  
-    const vec3 A = _ray_o;
-    const vec3 m = A - P;
+// cylinder defined by extremes pa and pb, and radious ra
+bool zylinder(const Zylinder& _p, const Ray& _ray, vec3& _intersect, vec3& _normal, float& _t){   
 
-    const float md = dot(m, d);
-    const float nd = dot(n, d);
-    const float dd = dot(d, d);
+    const vec3 ro = _ray.orig;
+    const vec3 rd = _ray.dir;
+    const vec3 pa = _p.p1;
+    const vec3 pb = _p.p2;
+    const float ra = _p.radius;
 
-    // Test if segment fully outside either endcap of cylinder
-    if (md < 0.f && md + nd < 0.f) return false; // Segment outside ‘p’ side of cylinder
-    if (md > dd && md + nd > dd) return false;     // Segment outside ‘q’ side of cylinder
+    const vec3 ba = pb-pa;
 
-    const float nn = dot(n, n);
-    const float mn = dot(m, n);
-    const float a = dd * nn - nd * nd;
-    const float k = dot(m, m) - r * r;
-    const float c = dd * k - md * md;
-    float t = 0.f;
+    const vec3  oc = ro - pa;
 
-    if (abs(a) < EPSILON) {
-        // Segment runs parallel to cylinder axis
-        if (c > 0.f) return false; // ‘a’ and thus the segment lie outside cylinder
-        // Now known that segment intersects cylinder; figure out how it intersects
+    const float baba = dot(ba,ba);
+    const float bard = dot(ba,rd);
+    const float baoc = dot(ba,oc);
+    
+    const float k2 = baba            - bard*bard;
+    const float k1 = baba*dot(oc,rd) - baoc*bard;
+    const float k0 = baba*dot(oc,oc) - baoc*baoc - ra*ra*baba;
+    float h = k1*k1 - k2*k0;
 
-        if (md < 0.f) t = -mn / nn; // Intersect segment against ‘p’ endcap
-        else if (md > dd) t = (nd - mn) / nn; // Intersect segment against ‘q’ endcap
-        else t = 0.f; // ‘a’ lies inside cylinder
+    if( h<0.0 ) return false;
+    h = sqrt(h);
+    _t = (-k1-h)/k2;
 
+    if(_t < 0.f) return false;
+    // body
+    const float y = baoc + _t*bard;
+    if( y>0.0 && y<baba ){ 
+        _intersect = ro + _t*rd;
+        _normal = normalize((oc+_t*rd - ba*y/baba)/ra);
         return true;
     }
 
-    const float b = dd * mn - nd * md;
-    const float discr = b * b - a * c;
-
-    if (discr < 0.f) return false; // No real roots; no intersection
-
-    t = (-b - sqrt(discr)) / a;
-
-    if (t < 0.f || t > 1.f) return false; // Intersection lies outside segment
-
-    if (md + t * nd < 0.f) {
-        // Intersection outside cylinder on ‘p’ side
-        if (nd <= 0.f) return false; // Segment pointing away from endcap
-        t = -md / nd;
-        // Keep intersection if Dot(S(t) - p, S(t) - p) <= r^2
-        return k + 2 * t * (mn + t * nn) <= 0.f;
-    } else if (md + t * nd > dd) {
-        // Intersection outside cylinder on ‘q’ side
-        if (nd >= 0.f) return false; // Segment pointing away from endcap
-        t = (dd - md) / nd;
-        // Keep intersection if Dot(S(t) - q, S(t) - q) <= r^2
-        return k + dd - 2 * md + t * (2 * (mn - nd) + t * nn) <= 0.f;
+    // caps
+    _t = ( ((y<0.0) ? 0.0 : baba) - baoc)/bard;
+    if( abs(k1+k2*_t) < h){ 
+        _intersect = ro + _t*rd;
+        _normal = normalize(ba*sign(y)/baba);
+        return true;
     }
-
-    // Segment intersects cylinder between the end-caps; t is correct
-    
-    return true; 
+    return false; //no intersection
 };
 
-void cameraRay(vec3& _ray_o, vec3& _ray_d, int64_t _w, int64_t _h, int64_t _u, int64_t _v, float _fovX, float _fovY, const mat4& _cam, const vec3& _camPos) {
-    const float x = (static_cast<float>(2*_u - _w)/static_cast<float>(_w))*std::tan(_fovX);
-    const float y = (static_cast<float>(2*_v - _h)/static_cast<float>(_h))*std::tan(_fovY);
-    _ray_o = vec3(0.f);
-    //const vec4 t = _cam * vec4(x, y, -1.f, 1.f);
-    _ray_d = normalize(vec3(x, y, -1.f));
+[[nodiscard]] Ray cameraRay(const Camera& _cam, int64_t _u, int64_t _v) noexcept {
+    const float x = (static_cast<float>(2*_u - _cam.w)/static_cast<float>(_cam.w))*std::tan(_cam.fovX);
+    const float y = (static_cast<float>(2*_v - _cam.h)/static_cast<float>(_cam.h))*std::tan(_cam.fovY);
+    Ray ray;
+    ray.dir = _cam * vec3(x, y, 1.f);
+    ray.orig = _cam.pos;
+    return ray;
 };
 
-vec3 mul(const mat4& _cam, const vec3& _vec){
-    const vec4 t = _cam * vec4(_vec.x, _vec.y, _vec.z, 1.f);
-    return vec3(t.x, t.y, t.z);
-}
+template<class Col, class Depth, class Shapes, class Func, class Camera>
+void render(Col& _cb, Depth& _d, const Shapes& _shapes, Func _func, const Ray& _ray, const Ambiente& _amb, const DirLight& _light, const Camera& _cam, size_t _idx, size_t _idx_d) noexcept {
+    for (size_t k = 0; k < _shapes.size(); ++k) {
+        vec3 inter;
+        vec3 normal;
+        float t = 0.f;
+        if (_func(_shapes[k], _ray, inter, normal, t)) {
+            const float z = t;
+            if (z < _d[_idx_d]) {
+                _d[_idx_d] = z;
+
+                const float diff = std::max(dot(normal, _light.dir), 0.f);
+                const vec3 diffl = diff * _light.col * _light.strength;
+                const vec3 res = (_amb.col * _amb.strength + diffl) * _shapes[k].mat.col;
+                const vec3 resg = glm::pow(res, vec3(1.f / 2.2f));
+                _cb[_idx] = (unsigned char)(std::clamp(resg.x * 255.f, 0.f, 255.f));
+                _cb[_idx + 1] = (unsigned char)(std::clamp(resg.y * 255.f, 0.f, 255.f));
+                _cb[_idx + 2] = (unsigned char)(std::clamp(resg.z * 255.f, 0.f, 255.f));
+                _cb[_idx + 3] = 255;
+            }
+        }
+    }
+};
 
 int main() {
 
-    const int64_t w = 1000;
-    const int64_t h = 1000;
+    // ------------------- CAMERA -------------------
+    Camera cam (1000, 1000, glm::radians(45.f), 0.001f, 1000.f);
+    cam.pos = vec3(0.f, 0.f, -500.f);
+    cam.target = vec3(0.f);
+    cam.update();
 
-    const float fovX = glm::radians(65.f);
-    const float fovY = w / h * fovX;
+    // ------------------- LIGHT -------------------
+    DirLight light;
+    light.dir = -normalize(vec3(1.f));
+    light.col = vec3(1.f);
+    light.strength = 0.75f;
 
-    const vec3 camPos = vec3(0.f, 0.f, -500.f);
-    const mat4 cam = glm::lookAt(camPos, vec3(0.f, 0.f, 0.f), vec3(0.f, 0.1f, 0.f));
-    const mat4 cami = inverse(cam);
+    Ambiente ambiente;
+    ambiente.col = vec3(1.f);
+    ambiente.strength = 0.4f;
     
+    // ------------------- SHAPES -------------------
     std::vector<Zylinder> zyls;
-    zyls.push_back({100.f,  mul(cam, vec3(0.f, -250.f, 0.f)),  mul(cam, vec3(0.f, 250.f, 0.f)) });
+    zyls.push_back( {100.f, vec3(0.f, -200.f, 300.f), vec3(0.f, 200.f, -300.f), Material(vec3(0.9f, 0.25f, 1.f)) });
 
     std::vector<Sphere> spheres;
-    //spheres.push_back({400.f, mul(cam, vec3(0.f))});
+    spheres.push_back( {250.f, vec3(0.f), Material(vec3(0.3f, 0.25f, 0.3f)) });
 
     std::vector<Plane> planes;
+    planes.push_back( { normalize(vec3(0.f, -1.f, 1.f)), vec3(0.f), 100.f, 50.f, Material(vec3(0.1f, 0.25f, 1.f)) } );
 
-    std::vector<float> depth (w * h);
-    std::vector<unsigned char> col (w * h * 4);
-    //std::memset(col.data(), 200, col.size());
+    // ------------------- BUFFERS -------------------
+    std::vector<float> depth (cam.w * cam.h);
+    for(size_t i = 0; i < depth.size(); ++i)
+        depth[i] = std::numeric_limits<float>::infinity();
 
-    const vec3 lightDir = -normalize(vec3(-1.f));
-    float ambientStrength = 0.4;
-    const vec3 ambient = ambientStrength * vec3(0.f, 0.5f, 1.f);
-    
-    for(int64_t u = 0; u < w; ++u){
-       for(int64_t v = 0; v < h; ++v){
+    std::vector<unsigned char> col (cam.w * cam.h * 4);
+    for(size_t i = 0; i < col.size(); i+=4){
+        col[i] = 0;
+        col[i+1] = 0;
+        col[i+2] = 0;
+        col[i+3] = 255;
+    }
+
+    // ------------------- RENDER -------------------
+    for(int64_t u = 0; u < cam.w; ++u){
+       for(int64_t v = 0; v < cam.h; ++v){
             vec3 ray_o, ray_d;
-            cameraRay(ray_o, ray_d, w, h, u, v, fovX, fovY, cami, camPos);
-
-            const size_t idx_d = v * w + u;
+            
+            const size_t idx_d = v * cam.w + u;
             const size_t idx_c = 4*idx_d;
 
-            for(size_t k = 0; k < zyls.size(); ++k){
-                vec3 inter;
-                vec3 normal;
-                if(zylinder(zyls[k], ray_o, ray_d, inter, normal)){
-                    const float diff = std::max(dot(normal, lightDir), 0.f);
-                    const vec3 diffl = diff * vec3(1.f);
-                    const vec3 res = (ambient + diffl) * vec3(65.f / 255.f, 178.f / 255.f , 210.f / 255.f);
-                    const vec3 resg = glm::pow(res, vec3(1.f/2.2f));
-    
-                    //std::cout << idx_c << std::endl;
-                    col[idx_c] = (unsigned char)(std::clamp(resg.x * 255.f, 0.f, 255.f));
-                    col[idx_c + 1] = (unsigned char)(std::clamp(resg.y * 255.f, 0.f, 255.f));
-                    col[idx_c + 2] = (unsigned char)(std::clamp(resg.z * 255.f, 0.f, 255.f));
+            for(size_t ms = 0; ms < 3; ++ms){
 
-                   // col[idx_c] = (unsigned char)(std::clamp(std::abs(normal.x) * 255.f, 0.f, 255.f));
-                    //col[idx_c + 1] = (unsigned char)(std::clamp(std::abs(normal.y) * 255.f, 0.f, 255.f));
-                    //col[idx_c + 2] = (unsigned char)(std::clamp(std::abs(normal.z) * 255.f, 0.f, 255.f));
+                Ray ray = cameraRay(cam, u, v);
 
+                render(col, depth, zyls, zylinder, ray, ambiente, light, cam, idx_c, idx_d);
+                render(col, depth, planes, plane, ray, ambiente, light, cam, idx_c, idx_d);
+                render(col, depth, spheres, sphere, ray, ambiente, light, cam, idx_c, idx_d);
 
-                    col[idx_c + 3] = 255;
-                } else {
-                    col[idx_c] = 0;
-                    col[idx_c + 1] = 0;
-                    col[idx_c + 2] = 0;
-                    col[idx_c + 3] = 255;
-                }
             }
-
-            
 
         } 
     }
+
     
-    std::string filename = "test.png";
-    lodepng::encode(filename, col.data(), w, h);
+
+    std::string filename = "result_col.png";
+    lodepng::encode(filename, col.data(), cam.w, cam.h);
+
+    //std::string filename = "result_depth.png";
+    //lodepng::encode(filename, col.data(), w, h);
 
     return EXIT_SUCCESS;
 };
